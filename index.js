97aaed3f07219057deaaac8570a3a6c69d3fa52d
jlxip <jlxip.mc@gmail.com>
2017-11-12 19:07:48 +0100

    Loads of error checking!

--- index.js
+++ index.js
@@ -20,29 +20,43 @@ http.createServer((req, res) => {
 		var DH = crypto.createDiffieHellman(params.g, encodeType)
 		var cookies = parseCookies(req)
 		if('x' in cookies) {
-			var privKey = decodeURIComponent(parseCookies(req).x)
-			DH.setPrivateKey(privKey, encodeType)
-			var secret = DH.computeSecret(decodeURIComponent(params.r), encodeType)
+			try {
+				var privKey = decodeURIComponent(parseCookies(req).x)
+				DH.setPrivateKey(privKey, encodeType)
+				var secret = DH.computeSecret(decodeURIComponent(params.r), encodeType)
 
-			res.writeHead(200, {'Content-Type': 'text/html', 'Set-Cookie': 'x=deleted; expires=Thu, 01 Jan 1970 00:00:00 GTM'})
-			res.write('The secret is: '+secret.toString('base64'))
+				res.writeHead(200, {'Content-Type': 'text/html', 'Set-Cookie': 'x=deleted; expires=Thu, 01 Jan 1970 00:00:00 GTM'})
+				res.write('The secret is: '+secret.toString('base64'))
+			} catch(e) {
+				res.writeHead(200, {'Content-Type': 'text/html'})
+				res.write('Something went really wrong. Try loading the previous URL.')
+			}
 		} else {
 			res.writeHead(200, {'Content-Type': 'text/html'})
 			res.write('You don\'t have the \'x\' (private key) cookie.')
 		}
 	} else if('g' in params) {
-		var DH = crypto.createDiffieHellman(decodeURIComponent(params.g), encodeType)
-		var r = encodeURIComponent(DH.generateKeys(encodeType))
-		var x = encodeURIComponent(DH.getPrivateKey(encodeType))
-
 		res.writeHead(200, {'Content-Type': 'text/html', 'Set-Cookie': 'x='+x})
-		res.write('Don\'t access. Just share:<br>\nhttps://dh.jlxip.net/?g='+encodeURIComponent(params.g)+'&r='+r)
-	} else if('s' in params) {
-		var newDH = crypto.createDiffieHellman(parseInt(params.s))
-		var g = encodeURIComponent(newDH.getPrime(encodeType))
 
+		try {
+			var DH = crypto.createDiffieHellman(decodeURIComponent(params.g), encodeType)
+			var r = encodeURIComponent(DH.generateKeys(encodeType))
+			var x = encodeURIComponent(DH.getPrivateKey(encodeType))
+
+			res.write('Don\'t access. Just share:<br>\nhttps://dh.jlxip.net/?g='+encodeURIComponent(params.g)+'&r='+r)
+		} catch(e) {
+			res.write('The generator (\'g\' by GET) is not valid.')
+		}
+	} else if('s' in params) {
 		res.writeHead(200, {'Content-Type': 'text/html'})
-		res.write('Share and access:<br>\nhttps://dh.jlxip.net/?g='+g)
+		if(parseInt(params.s)) {
+			var newDH = crypto.createDiffieHellman(parseInt(params.s))
+			var g = encodeURIComponent(newDH.getPrime(encodeType))
+
+			res.write('Share and access:<br>\nhttps://dh.jlxip.net/?g='+g)
+		} else {
+			res.write('Wrong size. Choose a power of two. 512 for tests. 2048 for real scenarios (it might be slower to generate).')
+		}
 	} else {
 		res.writeHead(200, {'Content-Type': 'text/html'})
 		res.write('First, create a G value:<br>\nhttps://dh.jlxip.net/?s=2048')
